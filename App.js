55faddda240ebaf0bab348d6d8dae1ef8aeb9834
adrianamarcf2005-bit <adrianamarcf2005@gmail.com>
2026-07-19 12:37:23 -0600

    feat: integrar react-native-encrypted-storage y servicio seguro (#2)
    * feat: integrar react-native-encrypted-storage y servicio seguro
    
    * refactor: corregir detalles

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Importamos el servicio seguro 
 
 export default function App() {
+
+  // El useEffect se ejecuta automáticamente cuando la app se abre
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
